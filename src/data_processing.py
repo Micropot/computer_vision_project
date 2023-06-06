69f8d48f81808321eb9168da173a486152a86572
Micropot <arthur@lamard.org>
2023-06-06 11:08:49 +0200

    final proj

--- src/data_processing.py
+++ src/data_processing.py
@@ -21,11 +21,11 @@ class ImageManagement :
     def LectureFichierImage(self, NomFichierImage, Parameters):
         print ("Lectrue du fichier :", NomFichierImage)
         img = Image.open(NomFichierImage).convert("L")
-        '''img = img.resize((28, 28))
+        img = img.resize((28, 28))
         print("type image : ",type(img))
         self.DonneeImage = np.array(img)
         print("type donnée image : ",type(self.DonneeImage))
-        self.DonneeImage = self.DonneeImage.reshape(1,28,28)'''
+        self.DonneeImage = self.DonneeImage.reshape(1,28,28)
         now = datetime.datetime.now()
         currentDateTime = now.strftime("%Y_%m_%d_%H_%M_%S")
         Parameters.image_path = os.path.join(Parameters.SaveImageDir, str(currentDateTime+'.png'))
