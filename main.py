d22f90e155dc93f61272f3ac2a50f68ac2e6a090
Polina Shpineva <polina.shpineva@gmail.com>
2023-06-20 16:07:09 +0300

    updated

--- main.py
+++ main.py
@@ -1,19 +1,56 @@
 import glob
 import os.path
 import sys
+
+from custom_model import scan_photo
 from src.NN import NN
 import src.Parameters
 import src.file_reading
 import src.gui
 from src.gui import Tk
+import tkinter as tk
+from tkinter import messagebox, simpledialog, filedialog
+
 from src.data_processing import ImageManagement
+
+def save_prediction(correct_prediction, prediction_name):
+    if correct_prediction:
+        filename = str(prediction_name) + ".txt"
+        file_path = filedialog.asksaveasfilename(defaultextension=".txt", initialfile=filename)
+        if file_path:
+            with open(file_path, "w") as file:
+                file.write(str(prediction_name))
+                messagebox.showinfo("Success", "Prediction saved successfully!")
+        else:
+            messagebox.showinfo("Cancelled", "Prediction not saved.")
+
+def display_popup(prediction_name):
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+
+    message = "Is the prediction '{}' correct?".format(prediction_name)
+    confirmed = messagebox.askyesno("Confirmation", message)
+    if not confirmed:
+        correct_prediction = simpledialog.askstring("Correct Prediction", "Please enter the correct prediction:")
+        if correct_prediction:
+            save_prediction(True, correct_prediction)
+
+    save_prediction(confirmed, prediction_name)
+
+def button_click():
+    print("Button clicked!")
+    file_path = filedialog.askopenfilename()
+    print("Selected file:", file_path)
+    global pred
+    pred = scan_photo(file_path)
+
 
 #TODO : https://medium.com/analytics-vidhya/handwritten-digit-recognition-gui-app-46e3d7b37287
 def main(argv):
     proj_param = src.Parameters.Parameters()
     proj_param = src.file_reading.YAML_Reader(argv[1], proj_param)
 
-    #proj_param.Print()
+    # proj_param.Print()
 
     #******** TRAINING ********
     MyIA = NN()
@@ -26,10 +63,13 @@ def main(argv):
     #*********** GUI ************
     root = Tk()
     p = src.gui.Draw(root, proj_param)
+    button = tk.Button(root, text="Upload photo", command=button_click)
+    button.pack()
+
     root.mainloop()
 
 
-    #******** IMAGE PREDICTION**********
+    #******** IMAGE PREDICTION **********
     '''MyImage = ImageManagement()
 
     MyImage.DonneeImage = MyImage.LectureFichierImage(str(proj_param.SaveImageDir + 'raw_image.png'), proj_param)
@@ -40,6 +80,8 @@ def main(argv):
     #MyIA.Prediction(latest_file)
     MyIA.Prediction(str(proj_param.SaveImageDir + 'raw_image.png'))'''
 
+    #******** POP UP WINDOW WITH FILE SAVING **********
+    display_popup(pred)
 
 
 
